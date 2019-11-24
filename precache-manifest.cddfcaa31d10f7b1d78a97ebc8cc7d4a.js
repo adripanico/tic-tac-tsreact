b87e267ad2a89101f075ce53448e059de49ed0d9
Adrián Caballero <adrian@epiclabs.io>
2019-11-24 18:31:13 +0100

    Updates

---
+++ precache-manifest.cddfcaa31d10f7b1d78a97ebc8cc7d4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445ea511a195958674c5e15c14dd8ae3",
+    "url": "/tic-tac-tsreact/index.html"
+  },
+  {
+    "revision": "0ab532aedb41c1a4d88d",
+    "url": "/tic-tac-tsreact/static/css/main.a2f99861.chunk.css"
+  },
+  {
+    "revision": "00bdf4e5fd739ec5f21d",
+    "url": "/tic-tac-tsreact/static/js/2.686c042d.chunk.js"
+  },
+  {
+    "revision": "0ab532aedb41c1a4d88d",
+    "url": "/tic-tac-tsreact/static/js/main.66ad80bb.chunk.js"
+  },
+  {
+    "revision": "a2d0b51182f1d034f4eb",
+    "url": "/tic-tac-tsreact/static/js/runtime-main.b9fe39e5.js"
+  }
+]);
